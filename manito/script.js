24a15878cc82b3cb098da177de9e6076c7da055c
Nordenium <itsraxeira@gmail.com>
2020-08-12 02:35:35 +0700

    Add pass

---
+++ manito/script.js
@@ -0,0 +1,25 @@
+const button = document.querySelector("#login-button");
+const popup = document.querySelector("#vid-popup");
+const form = document.querySelector("#form");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+
+button.onclick = login;
+
+password.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        login();
+    }
+});
+
+function login() {
+    if (["lee haechan", "ryu yangyang"].includes(username.value.toLowerCase()) && password.value == "bfftilljannah") ||
+        (password.value == "txtfromnorden" && username.value)
+    ) {
+        popup.style.display = "block";
+        form.style.display = "none";
+    } else {
+        alert("Wrong Password! Try again.");
+    }
+}
