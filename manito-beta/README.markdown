0373b332501b7aabd24983159b73060e82c1ee71
Rido Ramadan <rido.ramadan@gmail.com>
2020-08-21 01:24:49 +0700

    Add write and read to Firebase: Cloud Firestore

--- manito-beta/README.markdown
+++
@@ -1,6 +0,0 @@
-# CSS Postcard
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/texxs/pen/xmJVgp](https://codepen.io/texxs/pen/xmJVgp).
-
-Forked from:  https://codepen.io/MetalEscape/pen/vLvBmR  who said:
-Experimenting with form styling and absolute positioning of elements.
---
+++ manito-beta/scipt.js
@@ -0,0 +1,43 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC8JH_xK-n3RPChWBWyoGrzD7N3vmQ7YbU",
+    authDomain: "flameseeker-56fb5.firebaseapp.com",
+    databaseURL: "https://flameseeker-56fb5.firebaseio.com",
+    projectId: "flameseeker-56fb5",
+    storageBucket: "flameseeker-56fb5.appspot.com",
+    messagingSenderId: "121065227260",
+    appId: "1:121065227260:web:02a46a9c0cbbfc86656879"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+// HTML related logic
+const sender = document.querySelector("#sender");
+const recipient = document.querySelector("#recipient");
+const message = document.querySelector("#message");
+const sendButton = document.querySelector("#send-button");
+
+
+var msg;
+
+function sendMessage() {
+    var jsonMessage = {
+        from: sender.value,
+        to: recipient.value,
+        message: message.value
+    };
+    msg = jsonMessage;
+
+    db.collection("norde-manito")
+        .add(jsonMessage)
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        })
+}
+
+sendButton.addEventListener("click", sendMessage);
