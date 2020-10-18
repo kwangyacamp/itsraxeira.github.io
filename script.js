c78baa3fe54eb99a9ac1e364d16b6c4fc1c10dd1
Nordenium <itsraxeira@gmail.com>
2020-10-18 17:57:55 +0700

    Edit home

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$('button').click(function(){
-  $('button').toggleClass('active');
-  $('.title').toggleClass('active');
-  $('nav').toggleClass('active');
+$(".option").click(function(){
+   $(".option").removeClass("active");
+   $(this).addClass("active");
+   
 });
